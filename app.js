cca07c9abfdd0a6130321dcc08eed0cb1d61f394
Erich B <erichbernhardt@hotmail.com>
2019-06-19 14:49:20 -0600

    stashing

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const text =  baffle(".data");
+const text = baffle(".data");
 text.set({
-  characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
-        speed: 100
+    characters: '░▒░ ░██░> ████▓ >█> ░/█>█ ██░░ █<▒ ▓██░ ░/░▒',
+    speed: 100
 });
 
 text.start();
@@ -20,20 +20,20 @@ $(document).ready(function () {
         e.preventDefault();
         search();
         var question = $("#query").val();
-    console.log(question)
-    // $("#search-form").empty()
-    var taste = {
-      "async": true,
-      "crossDomain": true,
-      "url": "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + question + "&type=band&info=1&limit=10&k=338293-Project1-8GU1MS4N&type%20=band",
-      "method": "GET",
-    };
-  
-    $.ajax(taste).done(function (data) {
-        console.log(data.Similar.Results);
-        for (var i = 0; i < data.Similar.Results.length; i++)
-          $("#suggestions").prepend("<p>" + data.Similar.Results[i].Name + "</p>" + "<p><a href='" + data.Similar.Results[i].wUrl + "'>" + data.Similar.Results[i].wUrl + "</a></p>" + "<p><a href='" +data.Similar.Results[i].yUrl + "'>" + data.Similar.Results[i].yUrl + "/a></p>");
-      });
+        console.log(question)
+        // $("#search-form").empty()
+        var taste = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=" + question + "&type=band&info=1&limit=10&k=338293-Project1-8GU1MS4N&type%20=band",
+            "method": "GET",
+        };
+
+        $.ajax(taste).done(function (data) {
+            console.log(data.Similar.Results);
+            for (var i = 0; i < data.Similar.Results.length; i++)
+                $("#suggestions").prepend("<p>" + data.Similar.Results[i].Name + "</p>" + "<p><a href='" + data.Similar.Results[i].wUrl + "'>" + data.Similar.Results[i].wUrl + "</a></p>" + "<p><a href='" + data.Similar.Results[i].yUrl + "'>" + data.Similar.Results[i].yUrl + "/a></p>");
+        });
 
 
 
@@ -86,10 +86,12 @@ function buildOutput(item) {
     var channelTitle = item.snippet.channelTitle;
     var videoDate = item.snippet.publishedAt;
 
-    return `<div class="col-md-5 video-thumbnail">
+    return `<div class="col-md-8 video-thumbnail">
                     <iframe width="440" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>
                     <hr>
+                    <div>
                     <h4>${title}</h4>
                     <p>${description}</p>
+                </div>
                 </div>`;
 }
